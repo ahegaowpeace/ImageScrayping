326d8396689b13200876bcd6b3cbc23809b00301
ahegaowpeace <50076569+ahegaowpeace@users.noreply.github.com>
2020-06-01 06:21:41 +0900

    Update image.py

--- nhentai_scrayping/image.py
+++ nhentai_scrayping/image.py
@@ -1,3 +1,16 @@
+########################################################################
+#				mkdir & cd			       #
+########################################################################
+import os
+import sys
+import shutil
+p = '【contentsディレクトリパス】'
+foldername = sys.argv[1]
+path = p + foldername
+os.makedirs(path)
+os.chdir(path)
+
+
 #fake user agent for forbiding clorer
 headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'}
 
@@ -5,7 +18,8 @@ headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/
 #requests.get(url, header(uafake))
 import requests
 def download_data(url):
-	response = requests.get(url,headers=headers)
+	#response = requests.get(url,headers=headers)
+	response = requests.get(url,headers=headers, timeout=(None, None))
 	if response.status_code == 200:
 		return response.content
 	else:
@@ -27,14 +41,18 @@ from bs4 import BeautifulSoup
 ##########################################################################
 #				target url                               #
 ##########################################################################
-url = "https://nhentai.net/g/273192/"
 num = 0
 
 ##########################################################################
 #			range:1 to last page + 1                         #
+#		sample:https://nhentai.net/g/272740/                     #
 ##########################################################################
-for nm in range(1,206): 
-	url = "https://nhentai.net/g/273192/"
+u = sys.argv[2]
+u = u[:-2]
+n = int(sys.argv[3])
+
+for nm in range(1,int(n) + 1): 
+	url = u
 	url2 = url + str(nm) + "/"
 	print(url2)
 
